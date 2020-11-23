2720581408d8edbfc74f970ff4e391921ebfca48
porubu0325 <57026003+porubu0325@users.noreply.github.com>
2020-11-23 14:45:49 +0900

    Global Test
    グローバルチャットを追加。
    https://qiita.com/coolwind0202/items/061a08b0608d94fcb552 を参考にしました。

--- discordbot.py
+++ discordbot.py
@@ -16,13 +16,35 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
-    
-
+ 
 
 @bot.command()
 async def Hello(ctx):
     await ctx.send('Hello World')
         
-        
+
+@bot.command
+async def on_message(message):
+    if message.author.bot:
+        return
+    GLOBAL_CH_NAME = "nandatoglobal"
+
+    if message.channel.name == GLOBAL_CH_NAME:
+
+        await message.delete()
+
+        channels = client.get_all_channels()
+        global_channels = [ch for ch in channels if ch.name == GLOBAL_CH_NAME]
+
+        embed = discord.Embed(title="nandatoglobal",
+            description=message.content, color=0x00bfff)
+
+        embed.set_author(name=message.author.display_name, 
+            icon_url=message.author.avatar_url_as(format="png"))
+        embed.set_footer(text=f"{message.guild.name} / {message.channel.name}",
+            icon_url=message.guild.icon_url_as(format="png"))
+
+        for channel in global_channels:
+            await channel.send(embed=embed)
+
 bot.run(token)
